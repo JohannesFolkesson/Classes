7b991052bb6740f36e2d6bf8f8b7420ec163ccfd
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-06 18:50:30 +0100

    pets

--- Practice-3-Pokemon/script.js
+++ Practice-3-Pokemon/script.js
@@ -17,8 +17,33 @@ class Pokemon {
         console.log(`${this.name} took ${amount} damage!`)
     }
 
-    
+    heal(amount) {
+        this.health += amount;
+        if(this.health > 100) this.health = 100;
+        console.log(`${this.name} healed for ${amount} health`)
+    }
+
+    showStatus() {
+        console.log(`${this.name} is a ${this.type}-Pokemon with ${this.health} HP.`)
+    }
 
 
 
 }
+
+
+    const Gengar = new Pokemon("Gengar", "Ghost", 100)
+    const Butterfree = new Pokemon("Butterfree", "Bug", 100)
+
+    Gengar.attack(20)
+    Butterfree.takeDamage(20)
+
+    Butterfree.attack(50)
+    Gengar.takeDamage(50)
+
+    Butterfree.heal(10)
+    Gengar.heal(25)
+
+    Gengar.showStatus()
+    Butterfree.showStatus()
+
