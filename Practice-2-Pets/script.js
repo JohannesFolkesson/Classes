9dd9fac3dc8bc28ed7a61025e32093de68c289d7
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-06 18:19:06 +0100

    pets

--- Practice-2-Pets/script.js
+++ Practice-2-Pets/script.js
@@ -22,16 +22,37 @@ class Pet {
     }
 
     eat(foodAmount) {
-        
+        this.energy += foodAmount * 3
+        if(this.energy > 100) this.energy = 100;
+        console.log(`${this.name} eats ${foodAmount} food and gains energy`)
 
     }
 
     sleep(hours) {
+        const energyGain = hours * 10;
+        this.energy += energyGain
+        if(this.energy > 100) this.energy = 100;
+        console.log(`${this.name} sleeps for ${hours} and is ready for another playtime`)
 
     }
 
     showStatus() {
-
+        console.log(`${this.name} is a ${this.species} with energylevel of ${this.energy}/100`)
     }
 
 }
+
+const dog1 = new Pet("Cleo", "dog", 90)
+const dog2 = new Pet("Zoey", "dog", 30)
+
+dog1.play(10)
+dog2.play(5)
+
+dog2.eat(3)
+dog1.eat(2)
+
+dog1.sleep(3)
+dog2.sleep(5);
+
+dog1.showStatus();
+dog2.showStatus();
