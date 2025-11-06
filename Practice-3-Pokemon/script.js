62e4215996cec347332803c9df6d6d9ff9d57db9
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-06 18:35:32 +0100

    pets

--- Practice-3-Pokemon/script.js
+++ Practice-3-Pokemon/script.js
@@ -1,3 +1,24 @@
 class Pokemon {
+
+    constructor(name, type, health) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+
+    }
+
+    attack() {
+        console.log(`${this.name} uses an attack!`)
+    }
+
+    takeDamage(amount) {
+        this.health -= amount
+        if(this.health < 0 ) this.health = 0;
+        console.log(`${this.name} took ${amount} damage!`)
+    }
+
     
+
+
+
 }
