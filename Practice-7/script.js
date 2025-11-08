c6e44d1953d4b76b7d583cea8fa2019376faac6f
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-08 11:45:49 +0100

    FUT CARDS

--- Practice-7/script.js
+++ Practice-7/script.js
@@ -1,2 +1,37 @@
+class Dog {
+    constructor(breed, run, slowDown) {
+        this.breed = breed;
+        this.run = run;
+        this.slowDown = slowDown;
+    }
 
+    speed(amount) {
+        
+        console.log(`${this.breed} springer i ${this.run} km/h och ökar med ${amount} km/h`)
+        this.run += amount
+    }
+
+    slow(amount) {
+        this.run -= amount
+        if(this.run < 0) this.run = 0;
+        console.log(`${this.breed} stannar upp med ${amount} km/h`)
+    }
+
+    showStatus() {
+        console.log(`Hunden är en ${this.breed} som för tillfället springer i ${this.run} km/h`)
+
+    }
+
+}
+
+const Hund1 = new Dog("Beagle", 20, 10)
+const Hund2 = new Dog("Pomeranian", 9, 2)
+
+Hund1.speed(2)
+Hund2.speed(4)
+
+Hund1.slow(5)
+
+Hund1.showStatus()
+Hund2.showStatus()
 
