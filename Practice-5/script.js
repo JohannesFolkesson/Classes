1bd90c84a41b960f6eca40616bfa79dbba138549
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-11-10 19:02:55 +0100

    bugghantering

--- Practice-5/script.js
+++ Practice-5/script.js
@@ -11,7 +11,6 @@ class Fisk {
         console.log(`${this.name} eats ${amount}kg food, and its new weight is ${this.weight.toFixed(1)}`)
 
     }
-
     swim(distance) {
         const lostWeight = distance * 0.01
         this.weight -=  lostWeight;
