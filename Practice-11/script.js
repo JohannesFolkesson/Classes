07c38ccfe5dd7367b9fc193be0af13f261a6ee8b
Johannes Folkesson <johannes.folkesson@chasacademy.se>
2025-12-01 14:15:18 +0100

    More classes

--- Practice-11/script.js
+++ Practice-11/script.js
@@ -5,14 +5,19 @@ const Velen = new Priest("Velen")
 const Rexxar = new Hunter("Rexxar")
 const Thrall = new Shaman("Thrall")
 
+const characters = [Jaina, Velen, Rexxar, Thrall]
+
 console.log(Jaina.basicInfo())
 console.log(Velen.basicInfo())
 console.log(Rexxar.basicInfo())
 console.log(Thrall.basicInfo())
 
 
+const randomChar = characters[Math.floor(Math.random() * characters.length)]
 
-console.log(Jaina.useSpell() + " Hits Thrall for 10 damage")
+console.log(Jaina.useSpell() + " Hits " + randomChar.name + " for 10 damage")
+
+console.log(Rexxar.useSpell() + " Crits for 20 damage")
 
 
 
